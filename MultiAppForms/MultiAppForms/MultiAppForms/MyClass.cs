5b0a043d0c3246b1860c706e7516210ba79b2108
Klauditha <claudia.villarroel@live.com>
2020-05-05 22:53:58 -0400

    ADD: Seccion tipos de proyectos

---
+++ MultiAppForms/MultiAppForms/MultiAppForms/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiAppForms
+{
+    public class MyClass
+    {
+        public MyClass() { 
+        
+        }
+
+        public string SayMyName(string name)
+        {
+            return $"Hello: {name} ";
+        }
+    
+    }
+}
